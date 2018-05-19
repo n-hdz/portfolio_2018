ab95c5730478f11ff7979e4c87b1fb0abbccdd65
n.hdz <inhc.dev@gmail.com>
2018-05-19 12:17:23 -0500

    Job Model added

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-#Updated 2018 Django portfolio
+# Updated 2018 Django portfolio
+
+Portfolio / Blog app to document Django learning path
 
  - 05/19/2018 Init Commit
--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    """ Describes individual past Jobs to display in CV"""
+    # Title
+    title = models.CharField(verbose_name='job title', max_length=50)
+    # Thumbnail for Job
+    image = models.ImageField(verbose_name='thumbnail', upload_to='img/')
+    # Job description
+    summary = models.CharField(verbose_name='description', max_length=140)
+    # Link to
+    link = models.URLField(verbose_name='link to project', blank=True)
