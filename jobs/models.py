da835a83f5d806f504d4c3be1d9419b1673c3bbf
n.hdz <inhc.dev@gmail.com>
2018-05-19 12:38:53 -0500

    Admin config

--- jobs/models.py
+++ jobs/models.py
@@ -8,5 +8,7 @@ class Job(models.Model):
     image = models.ImageField(verbose_name='thumbnail', upload_to='img/')
     # Job description
     summary = models.CharField(verbose_name='description', max_length=140)
+    # Date of Job
+    date = models.DateTimeField(verbose_name='date of job', null=True)
     # Link to
     link = models.URLField(verbose_name='link to project', blank=True)
