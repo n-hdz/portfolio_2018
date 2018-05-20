fe31cda2e275630338ab8f0c4adf042368e70f58
n.hdz <inhc.dev@gmail.com>
2018-05-20 13:04:40 -0500

    Updated models -> added Blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    """ Describes individual Blog entries """
+    # Title for Blog entry
+    title = models.CharField(verbose_name="entry title", max_length=50)
+    # Date for Blog entry
+    date = models.DateField(verbose_name="entry timestamp")
+    # Thumbnail for Blog entry
+    image = models.ImageField(verbose_name="entry thumbnail", upload_to='img/blogs')
+    # Blog description
+    summary = models.TextField(verbose_name="entry body")
--- jobs/models.py
+++ jobs/models.py
@@ -5,7 +5,7 @@ class Job(models.Model):
     # Title
     title = models.CharField(verbose_name='job title', max_length=50)
     # Thumbnail for Job
-    image = models.ImageField(verbose_name='thumbnail', upload_to='img/')
+    image = models.ImageField(verbose_name='thumbnail', upload_to='img/jobs')
     # Job description
     summary = models.CharField(verbose_name='description', max_length=140)
     # Date of Job
